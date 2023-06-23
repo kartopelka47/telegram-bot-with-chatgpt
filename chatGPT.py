b7fc73478db0d6f69b6fbedfa4d16f77cc244256
kartoplya <kartoplya213@gmail.com>
2023-06-23 17:52:22 +0300

    add async

--- chatGPT.py
+++ chatGPT.py
@@ -1,29 +1,48 @@
 import openai
 
-def write_request(token,request_text):
-    """
-    :type token: str
-    :type request_text: str
-    :return: receive text from ChatGPT
-    """
-    openai.api_key = token
-    MODEL = "gpt-3.5-turbo"
-    response = openai.ChatCompletion.create(
-        model=MODEL,
-        messages=[
-            {"role": "user", "content": request_text}
-            # {"role": "system", "content": "You should only answer speak in germany"}
-        ],
-        temperature=0,
-    )
+class GPT:
+    def __init__(self, token, request_text):
+        self.received_text = None
+        self.token = token
+        self.request_text = request_text
 
-    # response = openai.ChatCompletion.create(
-    #     model=MODEL,
-    #     messages=[
-    #         {"role": "user", "content": request_text},
-    #         {"role": "assistant", "content": "bobr"}
-    #     ],
-    #     temperature=0,
-    # )
-    received_text = response["choices"][0]["message"]["content"]
-    return received_text
+    async def write_request(self):
+        """
+        :type token: str
+        :type request_text: str
+        :return: receive text from ChatGPT
+        """
+        openai.api_key = self.token
+        MODEL = "gpt-3.5-turbo"
+        response = openai.ChatCompletion.create(
+            model=MODEL,
+            messages=[
+                {"role": "user", "content": self.request_text}
+                # {"role": "system", "content": "You should only answer speak in germany"}
+            ],
+            temperature=0
+        )
+        received_text = response["choices"][0]["message"]["content"]
+        self.received_text = received_text
+        return received_text
+    
+    
+    
+    # async def write_request(self, token, request_text):
+    #     """
+    #     :type token: str
+    #     :type request_text: str
+    #     :return: receive text from ChatGPT
+    #     """
+    #     openai.api_key = token
+    #     MODEL = "gpt-3.5-turbo"
+    #     response = openai.ChatCompletion.create(
+    #         model=MODEL,
+    #         messages=[
+    #             {"role": "user", "content": request_text}
+    #             # {"role": "system", "content": "You should only answer speak in germany"}
+    #         ],
+    #         temperature=0
+    #     )
+    #     received_text = response["choices"][0]["message"]["content"]
+    #     return received_text
--- main.py
+++ main.py
@@ -16,22 +16,21 @@ import user_data
 import menu
 import asyncio
 
-
-TELEGRAM_API_TOKEN = os.environ.get("TELEGRAM_API_TOKEN")#Telegram_API_Token
-OPENAI_API_TOKEN = os.environ.get("OPENAI_API_TOKEN")#openAI_API_Token
+TELEGRAM_API_TOKEN = os.environ.get("TELEGRAM_API_TOKEN")  # Telegram_API_Token
+OPENAI_API_TOKEN = os.environ.get("OPENAI_API_TOKEN")  # openAI_API_Token
 database = user_data.DataBase(user_data.DATA_FILE_PATH)
-
 
 storage = MemoryStorage()
 logging.basicConfig(level=logging.INFO)
+
 
 class States(StatesGroup):
     search_info = State()
 
 
 bot = Bot(token=TELEGRAM_API_TOKEN)
-dp = Dispatcher(bot,storage=storage)
-
+dp = Dispatcher(bot, storage=storage)
+# chatGPT = chatGPT.GPT(OPENAI_API_TOKEN, None)
 
 @dp.message_handler(types.ChatType.is_private, commands="search")
 async def search_command(message: types.Message):
@@ -49,13 +48,14 @@ async def search_command(message: types.Message):
         user_join_date = received_user_info[2]
         user = user_data.User(user_id, user_language, user_join_date)
 
-    States.search_info.set()
-    bot.send_message(user.id, localization.write_search_query[user.language])
+    await States.search_info.set()
+    await bot.send_message(user.id, localization.write_search_query[user.language])
 
 
 @dp.message_handler(state=States.search_info)
 async def search_query(message: types.Message, state: FSMContext):
     user = user_data.User(message.from_user.id, "en", str(message.date))
+
 
     # load data about current user
     if database.IsNewbie(user.id):
@@ -70,18 +70,30 @@ async def search_query(message: types.Message, state: FSMContext):
         user = user_data.User(user_id, user_language, user_join_date)
 
     if message.text == "/search":
-        search_command(message)
-        state.finish()
+        await search_command(message)
+        await state.finish()
         return
     elif message.text == "/start":
-        start_command(message)
-        state.finish()
+        await start_command(message)
+        await state.finish()
         return
 
-    bot.send_message(user.id, localization.processing_request[user.language])
-    chatGPT_text = chatGPT.write_request(OPENAI_API_TOKEN, message.text)
-    bot.send_message(user.id, chatGPT_text)
-    state.finish()
+    await bot.send_message(user.id, localization.processing_request[user.language])
+
+    chat = chatGPT.GPT(OPENAI_API_TOKEN, message.text)
+    chat.request_text = message.text
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(await chat.write_request())
+    loop.run_until_complete(task)
+    # request_task = await asyncio.create_task(chat.write_request())
+    # asyncio.run(request_task)
+    # request_task = await asyncio.create_task(chatGPT.write_request(OPENAI_API_TOKEN, message.text))
+    # chatGPT_text = asyncio.run(request_task)
+    print(chat.received_text)
+    await bot.send_message(user.id, chat.received_text)
+    # await bot.send_message(user.id, chat.received_text if chat.received_text is not None else "Повторіть запит пізніше")
+    await state.finish()
+
 
 @dp.message_handler(types.ChatType.is_private, commands="start")
 async def start_command(message: types.Message):
@@ -92,13 +104,15 @@ async def start_command(message: types.Message):
         database.dbCommands(f"""INSERT INTO `user`(`user_id`,`language`,`join_date`) 
                 VALUES ('{user.id}','{user.language}','{user.join_date}')""")
     else:
-        received_user_info = database.getUser(f"select `user_id`, `language`,`join_date` from `user` where `user_id` = '{user.id}'")
+        received_user_info = database.getUser(
+            f"select `user_id`, `language`,`join_date` from `user` where `user_id` = '{user.id}'")
         user_id = received_user_info[0]
         user_language = received_user_info[1]
         user_join_date = received_user_info[2]
         user = user_data.User(user_id, user_language, user_join_date)
 
-    bot.send_message(user.id, localization.choose_language[user.language], reply_markup=menu.get_language_menu())
+    await bot.send_message(user.id, localization.choose_language[user.language], reply_markup=menu.get_language_menu())
+
 
 @dp.callback_query_handler()
 async def callback_handler(callback_query: types.CallbackQuery):
@@ -120,8 +134,9 @@ async def callback_handler(callback_query: types.CallbackQuery):
         user.language = callback_query.data
         database.dbCommands(f"update `user` set `language`='{user.language}' where `user_id`='{user.id}'")
 
-        bot.send_message(user.id, localization.choosed_language[user.language])
+        await bot.send_message(user.id, localization.choosed_language[user.language])
         return
 
+
 if __name__ == '__main__':
-    asyncio.run(executor.start_polling(dp, skip_updates=True))
+    executor.start_polling(dp, skip_updates=True)
